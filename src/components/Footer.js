7dd5ff743214027e76584bfe0caa0c1aaa6e1c95
chaudhary64 <chiragchoudhary64@gmail.com>
2025-07-02 00:02:04 +0530

    Refactor Footer component for improved readability and formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,49 @@ export default function Footer() {
         {/* Logo + Company Section */}
         <div>
           <div className="flex items-center space-x-2 mb-4">
-            <div className="bg-black text-white text-xs font-bold px-2 py-1 rounded">UC</div>
+            <div className="bg-black text-white text-xs font-bold px-2 py-1 rounded">
+              UC
+            </div>
             <span className="font-semibold text-lg">Urban Company</span>
           </div>
           <div>
             <h3 className="font-semibold mb-2">Company</h3>
             <ul className="space-y-1">
-              <li><a href="#" className="hover:underline">About us</a></li>
-              <li><a href="#" className="hover:underline">Investor Relations</a></li>
-              <li><a href="#" className="hover:underline">Terms & conditions</a></li>
-              <li><a href="#" className="hover:underline">Privacy policy</a></li>
-              <li><a href="#" className="hover:underline">Anti-discrimination policy</a></li>
-              <li><a href="#" className="hover:underline">ESG Impact</a></li>
-              <li><a href="#" className="hover:underline">Careers</a></li>
+              <li>
+                <a href="#" className="hover:underline">
+                  About us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Investor Relations
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Terms & conditions
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Privacy policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Anti-discrimination policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  ESG Impact
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Careers
+                </a>
+              </li>
             </ul>
           </div>
         </div>
@@ -27,37 +57,87 @@ export default function Footer() {
         <div>
           <h3 className="font-semibold mb-2">For customers</h3>
           <ul className="space-y-1">
-            <li><a href="#" className="hover:underline">UC reviews</a></li>
-            <li><a href="#" className="hover:underline">Categories near you</a></li>
-            <li><a href="#" className="hover:underline">Contact us</a></li>
+            <li>
+              <a href="#" className="hover:underline">
+                UC reviews
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Categories near you
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact us
+              </a>
+            </li>
           </ul>
         </div>
 
         {/* For Professionals */}
         <div>
           <h3 className="font-semibold mb-2">For professionals</h3>
-          <a href="#" className="hover:underline">Register as a professional</a>
+          <a href="#" className="hover:underline">
+            Register as a professional
+          </a>
         </div>
 
         {/* Social Links */}
         <div>
           <h3 className="font-semibold mb-2">Social links</h3>
           <div className="flex space-x-3 mb-4">
-            <a href="#"><img src="/icons/twitter.svg" alt="Twitter" className="h-6 w-6" /></a>
-            <a href="#"><img src="/icons/facebook.svg" alt="Facebook" className="h-6 w-6" /></a>
-            <a href="#"><img src="/icons/instagram.svg" alt="Instagram" className="h-6 w-6" /></a>
-            <a href="#"><img src="/icons/linkedin.svg" alt="LinkedIn" className="h-6 w-6" /></a>
+            <a href="#">
+              <img src="/icons/twitter.svg" alt="Twitter" className="h-6 w-6" />
+            </a>
+            <a href="#">
+              <img
+                src="/icons/facebook.svg"
+                alt="Facebook"
+                className="h-6 w-6"
+              />
+            </a>
+            <a href="#">
+              <img
+                src="/icons/instagram.svg"
+                alt="Instagram"
+                className="h-6 w-6"
+              />
+            </a>
+            <a href="#">
+              <img
+                src="/icons/linkedin.svg"
+                alt="LinkedIn"
+                className="h-6 w-6"
+              />
+            </a>
           </div>
           <div className="space-y-2">
-            <a href="#"><img src="/badges/appstore.png" alt="App Store" className="h-10" /></a>
-            <a href="#"><img src="/badges/googleplay.png" alt="Google Play" className="h-10" /></a>
+            <a href="#">
+              <img
+                src="/badges/appstore.png"
+                alt="App Store"
+                className="h-10"
+              />
+            </a>
+            <a href="#">
+              <img
+                src="/badges/googleplay.png"
+                alt="Google Play"
+                className="h-10"
+              />
+            </a>
           </div>
         </div>
       </div>
 
       <div className="border-t border-gray-200 text-xs text-gray-500 py-4 px-6 text-center">
         <p>* As on December 31, 2024</p>
-        <p>© Copyright 2025 Urban Company Ltd. (formerly known as UrbanClap Technologies India Limited) All rights reserved. | CIN: U74140DL2014PTC274413</p>
+        <p>
+          © Copyright 2025 Urban Company Ltd. (formerly known as UrbanClap
+          Technologies India Limited) All rights reserved. | CIN:
+          U74140DL2014PTC274413
+        </p>
       </div>
     </footer>
   );
