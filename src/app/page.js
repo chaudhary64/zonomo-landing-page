fd29f09d986192648c7dfb322073dbe961462e0d
chaudhary64 <chiragchoudhary64@gmail.com>
2025-07-04 17:05:51 +0530

    refactor: remove unused About and slider components from Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,17 @@
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/Hero";
-import Slider from "@/components/UI/Slider";
-import MostBookedSlider from "@/components/MostBookedSlider";
 import SilkPlaneWrapper from "@/components/SilkPlaneWrapper";
 import HowitWorks from "./../components/HowitWorks";
 import WhyChooseUs from "./../components/WhyChooseUs";
 import FAQ from "@/components/Faq";
 import AIFeatures from "@/components/features";
-import About from "@/components/About";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <HeroSection />
-      <About />
-      {/* <Slider/> */}
-      {/* <MostBookedSlider /> */}
       <WhyChooseUs />
       <AIFeatures />
       <HowitWorks />
