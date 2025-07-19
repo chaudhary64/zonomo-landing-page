5176667bc155962e2010f7d5aea9c5e17af26cdd
chaudhary64 <chiragchoudhary64@gmail.com>
2025-07-19 13:22:52 +0530

    feat: add AIFeatures component to Home page for enhanced functionality

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,13 @@ import FAQ from "@/components/Faq";
 import RoleCarousel from "@/components/TwoSection";
 import ServiceSlider from "./../components/ServiceList";
 import Coming_Soon from "@/components/Coming_Soon";
+import AIFeatures from "@/components/features/page";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <AIFeatures />
       <RoleCarousel />
       <ServiceSlider />
       <WhyChooseUs />
