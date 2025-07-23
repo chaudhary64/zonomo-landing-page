044dce49b233faf4a0eb4f92dd7bafca9bcc52cd
udayyyy-09 <udaychaudhary419@gmail.com>
2025-07-23 22:11:25 +0530

    cleaning up the code

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,10 @@ import Booking from "../components/Booking";
 import WhyChooseUs from "./../components/WhyChooseUs";
 import FAQ from "@/components/Faq";
 import RoleCarousel from "@/components/TwoSection";
-import ServiceSlider from "./../components/ServiceList";
 import Coming_Soon from "@/components/Coming_Soon";
 import AIFeatures from "@/components/features/page";
 import HeroParallaxDemo from './service-pro/page';
 import ServiceProvidersSection from "@/components/parallex";
-import LampDemo from "@/components/CTA";
 
 export default function Home() {
   return (
@@ -17,13 +15,10 @@ export default function Home() {
       <HeroSection />
       <AIFeatures />
       <RoleCarousel />
-      {/* <ServiceSlider /> */}
       <HeroParallaxDemo/>
       <ServiceProvidersSection />
       <WhyChooseUs />
-      {/* <LampDemo /> */}
       <Booking />
-      {/* <MacbookScroll/> */}
       <FAQ />
       <Coming_Soon />
     </>
